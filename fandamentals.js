a260bd1a5835d84201b8fcd7393b06a1cf2519f4
MohonSaha <practicewithmohon@gmail.com>
2023-03-13 11:28:10 +0600

    first commit

---
+++ fandamentals.js
@@ -0,0 +1,28 @@
+/*
+SIX JAVASCRIPT FANDAMENTALS
+
+1) How to declare a variable using let and const.
+2) How to write condition in javascript. Six basic conditional operator.    Multiple conditional operator (&&, ||)
+3) Javascript array.
+    i) Array index system.
+    ii) Array methods.
+    iii) Set and get array elements.
+4) Javascript loop.
+5) Function. 
+6) Javascript object.
+    i) Get value form object.
+            a) Direct by property.
+            b) Access via property name string.
+            c) Access via property name in a variable.
+    ii) Object methods.
+
+
+
+
+
+
+
+
+
+
+*/
